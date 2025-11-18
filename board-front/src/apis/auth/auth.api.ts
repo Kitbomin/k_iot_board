4d52c8687a6639d3ab4404a69788d5207cd66e47
Kitbomin <zzxxcc1433@naver.com>
2025-11-18 14:16:22 +0900

    Api fix

--- board-front/src/apis/auth/auth.api.ts
+++ board-front/src/apis/auth/auth.api.ts
@@ -2,27 +2,18 @@
 
 import type { LoginRequest, LoginResponse } from "@/types/auth.type";
 import { publicApi } from "../common/axiosInstance";
-import { API_ROUTES } from "../common/base.path";
 import type { ApiResponse } from "@/types/common/ApiResponse";
+import { AUTH_PATH } from "./auth.path";
 
 export const authApi = {
   login: async (req: LoginRequest): Promise<LoginResponse> => {
     // axios.메서드<메서드 반환타입>();
     const res = await publicApi.post<ApiResponse<LoginResponse>>(
       // 경로부터 작성함
-      API_ROUTES.AUTH.LOGIN,
-      req
+      AUTH_PATH.LOGIN, req
     );
 
     return res.data.data;
   },
 };
 
-// 로그인 요청 - 아래있는 코드를 위로 올린거임
-// export async function login(req: LoginRequest): Promise<LoginResponse> {
-//   const res = await publicApi.post<ApiResponse<LoginResponse>>(
-//     API_ROUTES.AUTH.LOGIN,
-//     req
-//   );
-//   return res.data.data;
-// }
