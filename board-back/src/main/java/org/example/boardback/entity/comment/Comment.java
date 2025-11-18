674a2142e4ae858bfd5f81606c94fcfe1f672282
Kitbomin <zzxxcc1433@naver.com>
2025-11-18 12:32:14 +0900

    refreshToken Entity

--- board-back/src/main/java/org/example/boardback/entity/comment/Comment.java
+++ board-back/src/main/java/org/example/boardback/entity/comment/Comment.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.boardback.entity.base.BaseTimeEntity;
 import org.example.boardback.entity.board.Board;
 import org.example.boardback.entity.user.User;
 
@@ -16,7 +17,7 @@ import org.example.boardback.entity.user.User;
 })
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Comment {
+public class Comment extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
     private Long id;
